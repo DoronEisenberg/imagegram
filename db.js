5868b41d7d15a2cdecd0185124b72de7dc6beb46
Doron <legotek@gmail.com>
2022-11-18 15:39:48 +0100

    i have uploaded the photos and text too, a bit of css

---
+++ db.js
@@ -0,0 +1,21 @@
+const spicedPg = require("spiced-pg");
+require("dotenv").config();
+
+const db = spicedPg(
+    `postgres:${process.env.USER}:${process.env.PASS}@localhost:5432/${process.env.DATABASE}`
+);
+
+module.exports.getImage = () => {
+    return db.query("SELECT * FROM images").then((result) => result.rows);
+};
+ /*
+module.exports.addContent = ({ url, username, title, description}) => {
+    return db
+        .query(
+            `INSERT INTO images ("url", "username", "title", "description") VALUES
+    VALUES ($1, $2, $3)
+    RETURNING id`,
+            [url, username, title, description]
+        )
+        .then((result) => result.rows[0]); 
+};*/
--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,51 @@
 import * as Vue from './vue.js';
+
+;
+
+Vue.createApp({
+    data() {
+        return {
+            headline: "New Instegram",
+            cardCSS: "all_cards",
+            images: [],
+            
+            //firstName: "",
+            //headlineCss: "headlineClass",
+            //count: 0,
+        }
+    },
+   
+   mounted() {
+        fetch("/images").then(res => res.json()).then(images => {
+            this.images = images;
+        })
+    }
+}).mount('#main');
+
+
+
+/*
+ methods: {
+        updateName: function (e) {
+            this.firstName = "Sven";
+        },
+        increaseCount: function () {
+            this.count++;
+        }
+
+    }
+    
+    const images = [
+    {
+        name: "Paris",
+        country: "France"
+    },
+    {
+        name: "Lisboa",
+        country: "Portugal"
+    },
+    {
+        name: "Berlin",
+        country: "Germany"
+    }
+],*/
